5b749610955d4d79dc9052bd7a298e577802c174
PabloGoez33 <luispablogoez@gmail.com>
2025-05-18 00:15:58 -0500

    Carpeta config

---
+++ config/APP.php
@@ -0,0 +1,7 @@
+<?php
+
+const SERVERURL = 'http://localhost/Campus_Connect/';
+
+const COMPANY = 'Campus Connect';
+
+date_default_timezone_set('America/Bogota');
---
+++ config/SERVER.php
@@ -0,0 +1,12 @@
+<?php
+
+const SERVER = 'localhost';
+const DB = 'Campus_Connect_DB';
+const USER = 'root';
+const PASSWORD = '';
+
+const SGDB = 'mysql:host=' . SERVER . ';dbname=' . DB;
+
+const METHOD = 'AES-256-CBC';
+const SECRET_KEY = 'CampusCOnnect';
+const SECRET_IV = '123456';
